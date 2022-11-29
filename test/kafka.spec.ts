76ed8b901e123c1504b1613d2bbfafcd64394a9f
João Pedro -JFI <jfi@certi.org.br>
2022-11-29 17:27:38 -0400

    add test

---
+++ test/kafka.spec.ts
@@ -0,0 +1,10 @@
+// import test from 'japa'
+
+// test.group('Producer', () => {
+//   test('can produce to a topic', (assert) => {})
+// })
+
+// test.group('Consumer', () => {
+//   test('can consume a topic', (assert) => {})
+//   test('can consume multiple topics', (assert) => {})
+// })
