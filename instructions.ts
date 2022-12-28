d788b658a98dba308390a5cdbe8c93f3484e6cf3
João Pedro -JFI <jfi@certi.org.br>
2022-12-28 11:13:31 -0400

    update instructions

--- instructions.ts
+++ instructions.ts
@@ -2,7 +2,9 @@ import * as sinkStatic from '@adonisjs/sink'
 import { ApplicationContract } from '@ioc:Adonis/Core/Application'
 import fs from 'fs'
 
-export default async function instructions(app: ApplicationContract, sink: typeof sinkStatic) {
+
+export default async function instructions(projectRoot: string, app: ApplicationContract, { logger }: typeof sinkStatic) {
+  logger.action('Root').succeeded(projectRoot)
   const adonisrcPath = `${app.appRoot}/.adonisrc.json`
   const adonisrc = JSON.parse(String(fs.readFileSync(adonisrcPath, 'utf8')))
   const commands = adonisrc.commands as string[]
@@ -12,18 +14,18 @@ export default async function instructions(app: ApplicationContract, sink: typeo
   ]
   commandsToInsert.forEach((command) => {
     if (commands.includes(command)) {
-      sink.logger.action('add').skipped(command, 'Command already exists in .adonisrc.json')
+      logger.action('add').skipped(command, 'Command already exists in .adonisrc.json')
     } else {
       commands.push(command)
-      sink.logger.action('add').succeeded(`${command} to .adonisrc.json`)
+      logger.action('add').succeeded(`${command} to .adonisrc.json`)
     }
   })
   adonisrc.commands = commands
   fs.writeFileSync(adonisrcPath, JSON.stringify(adonisrc, null, 2))
-  sink.logger.action('add').succeeded(`Commands added to .adonisrc.json`)
+  logger.action('add').succeeded(`Commands added to .adonisrc.json`)
 
   // run node ace generate:manifest
   await app.container.use('Adonis/Core/Manifest').generate()
 
-  sink.logger.action('create').succeeded('Commands registered')
+  logger.action('create').succeeded('Commands registered')
 }
